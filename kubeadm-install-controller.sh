95c464c6a84992cec62c53a9d23be901e7588202
bashfulrobot <dustin@bashfulrobot.com>
2024-01-28 17:28:06 -0800

    chore: remove cilium cli install Changes to be committed: 	modified:   kubeadm-install-controller.sh

--- kubeadm-install-controller.sh
+++ kubeadm-install-controller.sh
@@ -29,15 +29,6 @@ metallb_pool_ip6="${first_three_octets}.35"
 
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl
 
-# Install cilium CLI
-CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
-CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
-sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
-rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-
 ##### Add extra manifests
 mkdir -p /etc/kubernetes/manifests
 
